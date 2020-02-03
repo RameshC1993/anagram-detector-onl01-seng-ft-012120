8b1b48e94cca3ce32252f59e259cabd0d43b7453
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-02-03 20:42:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
         temp_arr = []
         words.each do |word|
             if word.length == @word.length 
-                if @word.sort == word.sort 
+                if @word.sort.join == word.sort.join
                     temp_arr << word
                 end
             end
