1ee056219f735c66f88524f515f31988c997c497
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-02-03 20:35:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,19 @@
-class Anagrams
-  attr_accessor :word 
+class Anagram
+    attr_accessor :word 
   
-  def initilaliz()
+  def initilaliz(word)
+    @word = word 
+  end
+
+    def self.match(words)
+        temp_arr = []
+        words.each do |word|
+            if word.length == @word.length 
+                if @word.sort == word.sort 
+                    temp_arr << word
+                end
+            end
+        end
+        return temp_arr
+    end
 end
