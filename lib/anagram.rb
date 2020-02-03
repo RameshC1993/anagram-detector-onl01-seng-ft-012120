8bc4c4af2d9becefa553a3527c70536e32ba1ebb
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-02-03 20:48:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,19 +1,15 @@
 class Anagram
-    attr_accessor :word 
+  attr_accessor :word
   
   def initialize(word)
-    @word = word 
+    @word = word
   end
-
-    def self.match(words)
-        temp_arr = []
-        words.each do |word|
-            if word.length == @word.length 
-                if @word.sort.join == word.sort.join
-                    temp_arr << word
-                end
-            end
-        end
-        return temp_arr
+  
+  def self.match(words)
+    arr = []
+    
+    words.each do |word|
+      
     end
+  end
 end
